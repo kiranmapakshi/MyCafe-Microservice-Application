a3465bb927881747a4df804d1fef73f3e3086131
kiranmapakshi <kiranmapakshi@gmail.com>
2020-05-08 23:20:36 +0530

    Eureka server and Zuul api gateway implementation.

--- MyCafe-Zuul/src/main/java/com/MyCafe/ZuulApplication.java
+++ MyCafe-Zuul/src/main/java/com/MyCafe/ZuulApplication.java
@@ -1,14 +1,16 @@
-package com.user.registration;
+package com.MyCafe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableZuulProxy
 @EnableDiscoveryClient
-public class UserRegistrationApplication {
+public class ZuulApplication {
     public static void main(String []args){
-        SpringApplication.run(UserRegistrationApplication.class, args);
+        SpringApplication.run(ZuulApplication.class, args);
     }
 }
